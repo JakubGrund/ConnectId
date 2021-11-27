c73e77b359e5be8f34e154759f349ee3b83de107
JakubGrund <jakub.grund@email.cz>
2021-11-27 14:31:53 +0100

    change base64 decode

--- ConnectId/Program.cs
+++ ConnectId/Program.cs
@@ -11,13 +11,13 @@ namespace ConnectId
 
         {
             // KID Key Azure
-            const string kidKeyAzure = "bW8ZcMjBCnJZS-ibX5UQDNStvx4";
+            const string kidKeyAzure = "bW8ZcMjBCnJZS-ibX5UQDNStvx4"; 
 
             // n => modulus from AD.Key Azure
-            const string n = "2a70SwgqIh8U-Shj_VJJGBheEVk2F4ygmMCRtKUAb1jMP6R1j5Mc5xaqhgzlWjckJI1lx4rha1oNLrdg8tJBxdm8V8xZohCOanJ52uAwoc6FFTY3VRLaUZSJ3zCXfuJwy4KvFHJUAuLhLj0hVeq-y10CmRJ1_MPTuNRJLdblSWcXyWYIikIRggQWS04M-QjR7571mX-Lu_eDs8xJVrnNFMVGRmFqf3EFD4QLNjW9JJj0m_prnTv41V_E8AA7MQZ12ip3u5aeOAQqGjVyzdHxvV9laxta6XWaM8QSTIu_Zav1-aDYExp99nCP4Hw0_Oom5vK5N88DB8VM0mouQi8a8Q";
+            string modulus = "2a70SwgqIh8U-Shj_VJJGBheEVk2F4ygmMCRtKUAb1jMP6R1j5Mc5xaqhgzlWjckJI1lx4rha1oNLrdg8tJBxdm8V8xZohCOanJ52uAwoc6FFTY3VRLaUZSJ3zCXfuJwy4KvFHJUAuLhLj0hVeq-y10CmRJ1_MPTuNRJLdblSWcXyWYIikIRggQWS04M-QjR7571mX-Lu_eDs8xJVrnNFMVGRmFqf3EFD4QLNjW9JJj0m_prnTv41V_E8AA7MQZ12ip3u5aeOAQqGjVyzdHxvV9laxta6XWaM8QSTIu_Zav1-aDYExp99nCP4Hw0_Oom5vK5N88DB8VM0mouQi8a8Q";
 
             // e => exponent for RSA from AD.Key Azure
-            const string e = "AQAB";
+            string exponent = "AQAB";
 
             //JWT Token Id.Token.Azure
             string azureIdTokenJWT = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6ImJXOFpjTWpCQ25KWlMtaWJYNVVRRE5TdHZ4NCJ9.eyJ2ZXIiOiIyLjAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTE4ODA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkL3YyLjAiLCJzdWIiOiJBQUFBQUFBQUFBQUFBQUFBQUFBQUFJMXFSeDVHclNpV005ckR6VVUyajFBIiwiYXVkIjoiZDliYWQxMTQtYTU2Yi00Y2ZlLWE0NGQtZGE1MDA1MzQ2NzkyIiwiZXhwIjoxNjM3ODM0MDQ1LCJpYXQiOjE2Mzc3NDczNDUsIm5iZiI6MTYzNzc0NzM0NSwidGlkIjoiOTE4ODA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkIiwibm9uY2UiOiJhenVyZSIsImFpbyI6IkRaTGVtT3lKWkE0NlhveE9jd0dSUDRNc1hoanBrcHFOYW9NbGp6NFdLenlPTTRLbjBpMVh6SjFUdyFjOTRYV09YKkoxZ1NkZ3FweEdIQmJVVU9oeXJ0SUV5bVpGM29NenA4cEU1QmZwTEN5UVlGQ043ZjQwNFhuc040aipQMEo3RGlGV2puMERGQ3RCcGpkdmZpYnE0MU9pOCpXcGdrY1oqaVl5cWp0bFZnclpzdVJUQlBXNkRCU1FZZzNoIW8xQkVnJCQifQ.ps0I7Aa1cuEcG0HC_B5LGTrMCLNgxjGRDkmKa1wC10EKdkfCwbo0eCafIrmcrw_DwvQaU0M0uvZPRzOJKYfOl7Jl2U_6fdkF2d2akSJixrR4J7g3bDpt5KssseqMiGepZeG5UNPWK6bKUL-AkVYW433eIwazgaNrmFu2KGohEuQOeddW2zUYhr6vVrGMREffld7EsRNx6LxyPo6-SJ8FJyJqO9KThq_AB4d8KWKjAdpyX9GGYATCQVWLi9fwl2UU3cpldXInogQbkRvFJgLXl2K9ePg__WlSrIXf1oIuxqipLAQjNuSVCYRFXYqPeeyfTwjWhksknBO3BfIwyUNvsw";
@@ -32,33 +32,43 @@ namespace ConnectId
 
             
 
-            byte[] bytesToSign = getBytes(string.Join(".", headerJWT, payloadJWT));  // output JSON UTF8
+            byte[] bytesToSign = GetBytes(string.Join(".", headerJWT, payloadJWT));  // output JSON UTF8
 
-            byte[] secret = getBytes("secret");  // output JSON UTF8
+            byte[] secret = GetBytes("secret");  // output JSON UTF8
 
-            var alg = new HMACSHA256(secret);
-            var hash = alg.ComputeHash(bytesToSign);
 
-            var signatureComputed = Base64UrlEncode(hash);
-
-            //exam
-            Console.WriteLine(signatureComputed);
-
+            
         }
 
-        private static byte[] getBytes(string value)
+        private static byte[] GetBytes(string value)
         {
             return Encoding.UTF8.GetBytes(value);
         }
 
-        private static string Base64UrlEncode(byte[] input)
+        public static RSAParameters SetPublicKey(string modulus, string exponent)
         {
-            var output = Convert.ToBase64String(input);
-            output = output.Split('=')[0];
-            output = output.Replace('+', '-');
-            output = output.Replace('/', '_');
-            return output;
+            RSAParameters result = new RSAParameters();
+            result.Modulus = Base64UrlDecode(modulus);
+            result.Exponent = Base64UrlDecode(exponent);
 
+            return result;
+        }
+
+        static byte[] Base64UrlDecode(string input)
+        {
+            string output = input;
+            output = output.Replace('-', '+'); // 62nd char of encoding
+            output = output.Replace('_', '/'); // 63rd char of encoding
+            switch (output.Length % 4) // Pad with trailing '='s
+            {
+                case 0: break; // No pad chars in this case
+                case 2: output += "=="; break; // Two pad chars
+                case 3: output += "="; break; // One pad char
+                default:
+                    throw new System.Exception(
+             "String is illegal!");
+            }
+            return Convert.FromBase64String(output); // Standard base64 decoder
         }
 
     }  
